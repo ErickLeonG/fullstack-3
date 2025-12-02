b5d296b4a91b6bb0d79c5e2d03b4d9086d1a7f7e
Erick Leon <erick.leon.gomez.03@gmail.com>
2025-12-02 15:45:58 +0200

    3.8

--- phonebook/index.js
+++ phonebook/index.js
@@ -27,7 +27,10 @@ let persons = [
 ]
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
